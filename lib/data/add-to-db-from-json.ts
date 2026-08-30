3923bc7c2197cd7bc2f97c2bb45306ce78d3d944
AviadDerli <aviad@workin.co.il>
2026-08-31 00:16:19 +0300

    mamar : rosh hashana beshabat

--- lib/data/add-to-db-from-json.ts
+++ lib/data/add-to-db-from-json.ts
@@ -1,9 +1,20 @@
 import { connectToMongodb } from '@/server/connect'
 import MamarModel from '@/server/models/mamar.model'
-import json from './קריאות-חדשות-בתורה.json'
+import json from './הלכות-ראש-השנה-החל-בשבת.json'
 
 
 export default async function addToDb() {
-   await connectToMongodb()
-   await MamarModel.create(json)
+   try {
+      console.log('Connecting to database...');
+      await connectToMongodb();
+      console.log('Connected. Inserting article...');
+      const res = await MamarModel.create(json);
+      console.log('✅ Article successfully added to database with ID:', res._id);
+      process.exit(0);
+   } catch (error) {
+      console.error('❌ Error adding to database:', error);
+      process.exit(1);
+   }
 }
+
+addToDb();
